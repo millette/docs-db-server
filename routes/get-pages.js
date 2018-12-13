ac740dc923022bb2e0d8f01740137ea4bda8acda
Robin Millette <robin@millette.info>
2018-12-13 12:52:31 -0500

    Paging.

--- routes/get-pages.js
+++ routes/get-pages.js
@@ -10,25 +10,31 @@ const makeSorter = (sort) => (a, b) => {
 
 const sorterLastMod = makeSorter("_updated")
 
-const format = ({ _id, _rev, _created, _updated }) => ({
-  _id,
-  _rev,
+const format = ({ _created, _updated, ...rest }) => ({
   _created: new Date(_created).toISOString(),
   _updated: new Date(_updated).toISOString(),
+  ...rest,
 })
 
 module.exports = async function(req, reply) {
   const docs = this.db.docMetas
   if (!docs.length) return []
-  if (!req.query.sort) return docs.slice(0, 10)
+  const page = req.query.page || 0
+  const d2 = docs.sort(sorterLastMod)
+  const { _rev, _updated } = d2.slice(-1)[0]
+  const etag = `${this.perPage}-${_rev}`
+  if (!req.query.sort) {
+    reply.lastMod(_updated).etag(etag)
+    return docs
+      .slice(page * this.perPage, page * this.perPage + this.perPage)
+      .map(format)
+  }
   const sort = "_" + req.query.sort
   if (!(sort in docs[0])) throw new Error("Unknown sort field")
-
-  const d2 = docs.sort(sorterLastMod)
   const d2a = sort === "_updated" ? d2 : docs.sort(makeSorter(sort))
   const d3 = req.query.desc ? d2a.reverse() : d2a
-
-  const { _rev, _updated } = d2.slice(-1)[0]
-  reply.lastMod(_updated).etag(_rev)
-  return d3.slice(0, 10).map(format)
+  reply.lastMod(_updated).etag(etag)
+  return d3
+    .slice(page * this.perPage, page * this.perPage + this.perPage)
+    .map(format)
 }
