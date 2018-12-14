89951aa652361e7138106a8d5a022aadecf6ddc3
Robin Millette <robin@millette.info>
2018-12-14 14:51:45 -0500

    Reformat.

--- index.js
+++ index.js
@@ -4,13 +4,8 @@
 const fastifyMod = require("fastify")
 
 module.exports = ({ config = {}, docs }) => {
-  const fastify = fastifyMod({
-    trustProxy: config.trustProxy,
-    logger: config.logger,
-  })
-  fastify.register(require("fastify-docs-db"), { config, docs })
-  return fastify.listen(
-    config.port || 3000,
-    config.hostname || process.env.HOSTNAME,
-  )
+  const { trustProxy, logger, port, hostname, ...cfg } = config
+  const fastify = fastifyMod({ trustProxy: trustProxy, logger: logger })
+  fastify.register(require("fastify-docs-db"), { config: cfg, docs })
+  return fastify.listen(port || 3000, hostname || process.env.HOSTNAME)
 }
