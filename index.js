d52357df5f463d0fc301e3f25388bc789402a5df
Robin Millette <robin@millette.info>
2018-12-13 22:24:45 -0500

    Extract to plugin, step 1.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 "use strict"
 
 // npm
-const DocsDb = require("docs-db")
-const fastifyMod = require("fastify")
+// const DocsDb = require("docs-db")
+// const fastifyMod = require("fastify")
+const fastify = require("fastify")({
+  // trustProxy: config.fastify.trustProxy,
+  // logger: config.fastify.logger,
+  trustProxy: "127.0.0.1",
+  logger: true,
+})
+
+/*
 const deepMerge = require("deepmerge")
 
 // core
@@ -59,8 +67,10 @@ const configDefault = {
 const nop = function() {
   return this
 }
+*/
 
 module.exports = ({ config = {}, docs }) => {
+  /*
   const n0 = Date.now()
   config = deepMerge(configDefault, config)
   const fastify = fastifyMod({
@@ -86,11 +96,9 @@ module.exports = ({ config = {}, docs }) => {
   // fastify.get("/api/delete/:page", deletePage)
   fastify.delete("/page/:page", deletePage)
 
-  /*
-  fastify.put("/api/page/:page", async (req, reply) => {
-    const page = req.params.page
-  })
-  */
+  // fastify.put("/api/page/:page", async (req, reply) => {
+    // const page = req.params.page
+  // })
 
   fastify.log.info(`Done starting (${(Date.now() - n0) / 1000}s).`)
   const now = Date.now()
@@ -100,5 +108,9 @@ module.exports = ({ config = {}, docs }) => {
   fastify.decorate("perPage", config.main.perPage)
   fastify.decorateReply("pagination", pagination)
   fastify.log.info(`Done reading (${(Date.now() - now) / 1000}s).`)
-  return fastify.listen(config.main.port, config.main.hostname)
+  */
+  fastify.register(require("./plugin"), { config, docs })
+
+  // return fastify.listen(config.main.port, config.main.hostname)
+  return fastify.listen(3000, process.env.HOSTNAME)
 }
