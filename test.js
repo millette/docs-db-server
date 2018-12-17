398f98724730892a5cfa17b7d19c912f72eababe
Robin Millette <robin@millette.info>
2018-12-17 17:25:36 -0500

    Basic unit tests.

---
+++ test.js
@@ -0,0 +1,19 @@
+// npm
+import test from "ava"
+import got from "got"
+
+// self
+import srv from "."
+
+test("me testo uno", async (t) => {
+  const base = await srv({
+    docs: require("./out-fake.json"),
+  })
+
+  const { body } = await got(`${base}/pages`, { json: true })
+  t.is(body.length, 24)
+  const b2 = await got(`${base}/page/${body[0]._id}`, { json: true })
+  t.is(b2.body._rev, body[0]._rev)
+  t.truthy(b2.body.title)
+  t.truthy(b2.body.content)
+})
